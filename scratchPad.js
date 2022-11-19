a115ddecfb92738adaec5c1124312de20aa5fc9f
Gus.williams95@gmail.com <Gus.williams95@gmail.com>
2022-11-19 15:12:50 -0500

    first commit to main

---
+++ scratchPad.js
@@ -0,0 +1,16 @@
+// const h1 = document.querySelector("h1");
+const happyLetters = document.querySelectorAll(".letter");
+
+function randomRGB() {
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+
+	return `rgb(${r},${g},${b})`;
+}
+
+setInterval(function () {
+	for (let letter of happyLetters) {
+		letter.style.color = randomRGB();
+	}
+}, 600);
